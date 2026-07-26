f6a800d8d70a9b714f5bfebdaf0853bafc0aef7b
Fritz <fitvlad1@gmail.com>
2026-07-26 11:05:31 +0300

    added animations, enum states

--- game.c
+++ game.c
@@ -2,6 +2,7 @@
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_scancode.h>
 #include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_surface.h>
 #include <SDL3/SDL_timer.h>
 #include <SDL3_image/SDL_image.h>
 #include <stdlib.h>
@@ -29,23 +30,137 @@ void Destroy(SDL_Window* window, SDL_Renderer* renderer){
 void HandlePlayerIn(const bool* keys, Player* cat, float speed, float dt){
     
     speed = speed * dt;
-    if(keys[SDL_SCANCODE_LSHIFT] && (keys[SDL_SCANCODE_LEFT] || keys[SDL_SCANCODE_RIGHT] || keys[SDL_SCANCODE_UP] || keys[SDL_SCANCODE_DOWN])){
+
+    bool shift = keys[SDL_SCANCODE_LSHIFT];
+    bool left = keys[SDL_SCANCODE_LEFT];
+    bool right = keys[SDL_SCANCODE_RIGHT];
+    bool up = keys[SDL_SCANCODE_UP];
+    bool down = keys[SDL_SCANCODE_DOWN];
+    bool moving = left || right || up || down;
+
+    //Running
+    if(shift && moving){
         speed*=2;
     }
-    if(keys[SDL_SCANCODE_LEFT]){
+
+    //Movement
+    if(left){
         cat->x-=speed;
+        cat->last_facing=FACING_LEFT;
     }
-    if(keys[SDL_SCANCODE_RIGHT]){
+    if(right){
         cat->x+=speed;
+        cat->last_facing=FACING_RIGHT;
     }
-    if(keys[SDL_SCANCODE_UP]){
+    if(up){
         cat->y-=speed;
+        cat->last_facing=FACING_UP;
     }
-    if(keys[SDL_SCANCODE_DOWN]){
+    if(down){
         cat->y+=speed;
+        cat->last_facing=FACING_DOWN;
     }
-    
+
+    PlayerState new_state;
+    //Whats the next state
+    if(left && shift){
+        new_state=STATE_RUN_LEFT;
+    }
+    else if(right && shift){
+        new_state=STATE_RUN_RIGHT;
+    }
+    else if (up && shift){
+        new_state=STATE_RUN_UP;
+    }
+    else if (down && shift){
+        new_state = STATE_RUN_DOWN;
+    }
+    else if (left){
+        new_state=STATE_WALK_LEFT;
+    }
+    else if (right){
+        new_state = STATE_WALK_RIGHT;
+    }
+    else if (up){
+        new_state=STATE_WALK_UP;
+    }
+    else if (down){
+        new_state=STATE_WALK_DOWN;
+    }
+    else{
+        new_state=STATE_IDLE;
+    }
+
+    if(cat->direction!=new_state){
+        cat->direction=new_state;
+        cat->current_frame=0;
+        cat->anim_timer=0.0;
+
+        switch(new_state){                                               
+                case STATE_IDLE:                                             
+                    switch(cat->last_facing){                                
+                        case FACING_LEFT:  cat->current_row = 21; break;    
+                        case FACING_RIGHT: cat->current_row = 22; break;    
+                        case FACING_UP:    cat->current_row = 20; break;    
+                        default:           cat->current_row = 19;  break;    
+                    }                                                        
+                    cat->total_frames = 5;                                  
+                    cat->anim_speed = 0.3;                                   
+                    break;                                                   
+                case STATE_WALK_LEFT:                                        
+                    cat->current_row = 4;                                    
+                    cat->total_frames = 6;                                   
+                    cat->anim_speed = 0.2;                                   
+                    break;                                                   
+                case STATE_WALK_RIGHT:                                       
+                    cat->current_row = 5;                                  
+                    cat->total_frames = 6;                                 
+                    cat->anim_speed = 0.2;                                   
+                    break;                                                   
+                case STATE_WALK_UP:                                          
+                    cat->current_row = 3;                                  
+                    cat->total_frames = 6;                                 
+                    cat->anim_speed = 0.2;                                   
+                    break;                                                   
+                case STATE_WALK_DOWN:                                        
+                    cat->current_row = 2;                                  
+                    cat->total_frames = 6;                                 
+                    cat->anim_speed = 0.2;                                   
+                    break;                                                   
+                case STATE_RUN_LEFT:                                         
+                    cat->current_row = 11;                                  
+                    cat->total_frames = 5;                                 
+                    cat->anim_speed = 0.1;                                   
+                    break;                                                   
+                case STATE_RUN_RIGHT:                                        
+                    cat->current_row = 10;
+                    cat->total_frames = 5;
+                    cat->anim_speed = 0.1;  
+                    break;
+                case STATE_RUN_UP:
+                    cat->current_row = 9;
+                    cat->total_frames = 4;
+                    cat->anim_speed = 0.1;  
+                    break;
+                case STATE_RUN_DOWN:
+                    cat->current_row = 8;
+                    cat->total_frames = 4;
+                    cat->anim_speed = 0.1;  
+                    break;
+            }             
+        }
 }
+
+
+
+
+
+
+
+
+
+
+  
 
 void KeepInside(Player* player, float min, float max_x, float max_y){
     //checking the player is still in the window, or where we want
@@ -72,12 +187,22 @@ void RunGame(SDL_Renderer* renderer){
     Player cat;
     cat.x=100.0;
     cat.y=100.0;
-    cat.width=150.0;
-    cat.height=150.0;
-    cat.texture=IMG_LoadTexture(renderer, "assets/cat.png");
+    cat.width=192.0;
+    cat.height=192.0;
+    cat.current_frame=0;
+    cat.current_row=37; 
+    cat.total_frames=11;
+    cat.anim_timer=0.0;
+    cat.anim_speed=0.3;  //0.3 good speed for 11 frames
+    cat.direction = STATE_IDLE;  //for idle
+    cat.last_facing=FACING_DOWN;  //for idle
+
+    cat.texture=IMG_LoadTexture(renderer, "assets/cat_sheet.png");
+    
     if(!cat.texture){
         SDL_Log("Image fail: %s", SDL_GetError());
     }
+    SDL_SetTextureScaleMode(cat.texture, SDL_SCALEMODE_NEAREST);
 
     Uint64 last = SDL_GetPerformanceCounter();  //For delta time
 
@@ -98,7 +223,12 @@ void RunGame(SDL_Renderer* renderer){
         float speed = 230.0;
         HandlePlayerIn(keys, &cat, speed, dt);      //Make the player move by the arrows
         KeepInside(&cat, 0, WIDTH, HEIGHT);     //Keep player in bounds
-
+        
+        cat.anim_timer+=dt;
+        if(cat.anim_timer>=cat.anim_speed){
+            cat.current_frame=(cat.current_frame+1)%cat.total_frames;
+            cat.anim_timer=0;
+        }
 
 
 
@@ -108,8 +238,9 @@ void RunGame(SDL_Renderer* renderer){
         SDL_RenderClear(renderer);
 
         //Drawing the player
+        SDL_FRect src = {cat.current_frame * 64, cat.current_row * 64, 64, 64};
         SDL_FRect dest = {cat.x, cat.y, cat.width, cat.height};     //Float rectangle, position and how big
-        SDL_RenderTexture(renderer, cat.texture, NULL, &dest);
+        SDL_RenderTexture(renderer, cat.texture, &src, &dest);
 
 
 
--- game.h
+++ game.h
@@ -4,16 +4,44 @@
 #define WIDTH 1600
 
 //Struct of the player with it's attributes
+#include <SDL3/SDL.h>
+#include <stdbool.h>
+
+typedef enum{
+    STATE_IDLE,
+    STATE_WALK_LEFT,
+    STATE_WALK_RIGHT,
+    STATE_WALK_UP,
+    STATE_WALK_DOWN,
+    STATE_RUN_LEFT,
+    STATE_RUN_RIGHT,
+    STATE_RUN_UP,
+    STATE_RUN_DOWN,
+}PlayerState;
+
+typedef enum{
+    FACING_DOWN,//idle
+    FACING_LEFT,
+    FACING_RIGHT,
+    FACING_UP,
+}FacingDir;
+
+
+
+
+
 typedef struct{
     float x, y;
     float width, height;
     SDL_Texture* texture;
+    int current_frame, current_row;
+    int total_frames;
+    float anim_timer;
+    float anim_speed;
+    PlayerState direction; 
+    FacingDir last_facing;
 }Player;
 
-    
-#include <SDL3/SDL.h>
-#include <stdbool.h>
-    
     void Init(SDL_Window** window, SDL_Renderer** renderer);
     void Destroy(SDL_Window* window, SDL_Renderer* renderer);
     void RunGame(SDL_Renderer* renderer);
